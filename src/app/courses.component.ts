2c3b242efddb43e654eb2979d85c0a6b76ca0d09
Hamza Habib <hhabib@ssidecisions.com>
2022-02-11 12:20:20 +0500

    Attribute bind and bootstrap package

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,19 +5,24 @@ import { CoursesService } from './courses.service';
   selector: 'courses',
   template: `
     <h2>{{ getTiile() }}</h2>
+    <h2 [textContent]="title"></h2>
     <ul>
       <li *ngFor="let course of courses">{{ course }}</li>
     </ul>
     <img src="{{ imageUrl }}" />
     <img [src]="imageUrl" />
+    <table>
+      <td [colSpan]="colSpan"></td>
+    </table>
   `,
 })
 // propertu Binding <img [src]="imageUrl" ONLY FROM COMPONENT->DOM
 // String Interpolation img src="{{ imageUrl }}" />
 export class CoursesComponent {
-  private title: string = 'New Title';
+  public title: string = 'New Title';
   public courses;
   public imageUrl = 'https://picsum.photos/200/300';
+  public colSpan = 2;
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
   }
