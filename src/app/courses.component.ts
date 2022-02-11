bde5537616f2829396047dcba6cff27bec4663ca
Hamza Habib <hhabib@ssidecisions.com>
2022-02-11 11:35:16 +0500

    Service with generic method

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,16 +1,22 @@
-import { Component } from "@angular/core";
-
+import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
 
 @Component({
-  selector:'courses',
-  template:'<h2>{{getTiile( )}}</h2>'
+  selector: 'courses',
+  template: `
+    <h2>{{ getTiile() }}</h2>
+    <ul>
+      <li *ngFor="let course of courses">{{ course }}</li>
+    </ul>
+  `,
 })
-export class CoursesComponent{
-  private title:string = "New Title";
-
-  getTiile():string{
+export class CoursesComponent {
+  private title: string = 'New Title';
+  public courses;
+  constructor(service: CoursesService) {
+    this.courses = service.getCourses();
+  }
+  getTiile(): string {
     return this.title;
   }
-
-
 }
