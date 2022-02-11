8e02a912fd648b51a7e485951b0a6a32bf95da20
Hamza Habib <hhabib@ssidecisions.com>
2022-02-11 11:09:43 +0500

    Added string interpolation and new component

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,9 +3,14 @@ import { Component } from "@angular/core";
 
 @Component({
   selector:'courses',
-  template:'<h2>Courses</h2>'
+  template:'<h2>{{getTiile( )}}</h2>'
 })
 export class CoursesComponent{
+  private title:string = "New Title";
+
+  getTiile():string{
+    return this.title;
+  }
 
 
 }
