9f74ea2ba1e36fe3259c03bb30addde2bb252371
Hamza Habib <hhabib@ssidecisions.com>
2022-02-11 11:44:25 +0500

    Property binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -8,11 +8,16 @@ import { CoursesService } from './courses.service';
     <ul>
       <li *ngFor="let course of courses">{{ course }}</li>
     </ul>
+    <img src="{{ imageUrl }}" />
+    <img [src]="imageUrl" />
   `,
 })
+// propertu Binding <img [src]="imageUrl" ONLY FROM COMPONENT->DOM
+// String Interpolation img src="{{ imageUrl }}" />
 export class CoursesComponent {
   private title: string = 'New Title';
   public courses;
+  public imageUrl = 'https://picsum.photos/200/300';
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
   }
