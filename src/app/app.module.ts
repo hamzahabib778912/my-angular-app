e3a837af00467f21fd595110c8119fb2aea5b7bb
Hamza Habib <hamzahabib778912@gmail.com>
2022-02-14 09:58:41 +0500

    Basic bootstrap form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { CoursesService } from './courses.service';
 import { FormsModule } from '@angular/forms';
 import { PanelComponent } from './panel/panel.component';
 import { InputFormatDirective } from './input-format.directive';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
-  declarations: [AppComponent, CoursesComponent, CourseComponent, PanelComponent, InputFormatDirective],
+  declarations: [AppComponent, CoursesComponent, CourseComponent, PanelComponent, InputFormatDirective, ContactFormComponent],
   imports: [BrowserModule, FormsModule],
   providers: [CoursesService],
   bootstrap: [AppComponent],
